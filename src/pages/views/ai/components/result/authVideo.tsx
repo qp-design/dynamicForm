1ba107e6aca7d74f28ca678a708cb2218e613854
秦鹏 <qinpeng@qinpengdeMacBook-Pro.local>
2021-09-18 14:25:24 +0800

    fix(authVideo): 高度一屏幕显示

--- src/pages/views/ai/components/result/authVideo.tsx
+++ src/pages/views/ai/components/result/authVideo.tsx
@@ -1,5 +1,5 @@
 import { videoAuthType } from "libs/types/video";
-import { ReactNode, useEffect } from "react";
+import { ReactNode, useEffect, useCallback, useState } from "react";
 
 export default function AuthVideo({
   video,
@@ -8,6 +8,14 @@ export default function AuthVideo({
   video: videoAuthType | null;
   children: ReactNode;
 }) {
+  const [height, setHeight] = useState<string>("0px");
+  const measuredRef = useCallback((node) => {
+    console.log(node);
+    if (node !== null) {
+      const { y } = node.getBoundingClientRect();
+      setHeight(`calc(100vh - ${y + 15}px)`);
+    }
+  }, []);
   useEffect(() => {
     // @ts-ignore
     var player = new Aliplayer(
@@ -30,7 +38,7 @@ export default function AuthVideo({
   }, [video]);
 
   return (
-    <div style={{ position: "relative", height: "100%" }}>
+    <div style={{ position: "relative", height }} ref={measuredRef}>
       {children}
       <div
         className="prism-player"
