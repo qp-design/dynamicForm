b840a0d5e870341f58bc5fc28bb89b095db8e8ed
devil <qinpeng_zong@163.com>
2021-10-28 00:26:11 +0800

    feat: 单选按钮位置样式

--- src/components/dynamic-form/fields/RadioGroupField.tsx
+++ src/components/dynamic-form/fields/RadioGroupField.tsx
@@ -1,20 +1,25 @@
-import { Radio } from "antd";
+import { Radio, Space } from "antd";
 import ComplexFields from "./ComplexFields";
 
+type emums = "vertical" | "horizontal";
 export default function RadioGroupField({
   options,
+  direction = "horizontal",
   ...extraProps
 }: {
+  direction: emums;
   options: Array<any>;
 }) {
   return (
     <Radio.Group {...extraProps}>
-      {options.map((item, idx) => (
-        <Radio key={idx} value={item.value}>
-          {item.label}
-          {item.suffix ? <ComplexFields innerForm={item.suffix} /> : null}
-        </Radio>
-      ))}
+      <Space direction={direction}>
+        {options.map((item, idx) => (
+          <Radio key={idx} value={item.value}>
+            {item.label}
+            {item.suffix ? <ComplexFields innerForm={item.suffix} /> : null}
+          </Radio>
+        ))}
+      </Space>
     </Radio.Group>
   );
 }
--- src/pages/formConfig/three.tsx
+++ src/pages/formConfig/three.tsx
@@ -1,11 +1,12 @@
 import { FieldType } from "libs/types/formField";
 
-const innerForm: Array<FieldType> = [
+const innerForm = (value: number): Array<FieldType> => [
   {
     colon: false,
     name: "testOne",
     type: "checkbox",
     label: " ",
+    calIsVisible: (getFieldValue) => getFieldValue("user") === value,
     prefixIcon: (
       <div>
         内蒙古自治区疾控中心副主任范蒙光表示，额济纳旗疫情防控难点体现在：医疗资源比较匮乏；病例的病情非常隐匿；由于地广人稀，口岸封控难度较大。
@@ -24,16 +25,17 @@ const fieldsForm: Array<FieldType> = [
     type: "radioGroup",
     label: "说明",
     extraProps: {
+      direction: "vertical",
       options: [
         {
           label: "1说明大小",
           value: 1,
-          suffix: innerForm,
+          suffix: innerForm(1),
         },
         {
           label: "2说明大小",
           value: 2,
-          suffix: innerForm,
+          suffix: innerForm(2),
         },
       ],
       a: "123",
